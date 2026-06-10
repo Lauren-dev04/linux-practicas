94bc8b2bd8e5cbc8552819dd71c6d4d588183bb7
Lauren-dev04 <sr.aureliannj@gmail.com>
2026-06-10 19:37:08 +0200

    feat: añadir estructura de practicas + primer script en bash

---
+++ practicas/bash/scripts/Primer_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "========================================"
+echo "    MI PRIMER SCRIPT EN BASH"
+echo "========================================"
+echo ""
+
+echo "Fecha y hora actual: $(date)"
+echo "Usuario actual: $(whoami)"
+echo "Directorio actual: $(pwd)"
+echo "Version del kernel: $(uname -r)"
+echo ""
+
+echo "!Felicidades! Tu primer script funcioa correctamente"
